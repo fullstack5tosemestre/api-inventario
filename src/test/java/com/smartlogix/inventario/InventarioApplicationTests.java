eb99d7a8d47f210ace768394ebf056c4ef644805
dainz <mbenjaminarancibia@gmail.com>
2026-03-29 19:06:12 -0300

    add test profile so it doesnt try connecting to db when running tests or compiling

--- src/test/java/com/smartlogix/inventario/InventarioApplicationTests.java
+++ src/test/java/com/smartlogix/inventario/InventarioApplicationTests.java
@@ -2,8 +2,10 @@ package com.smartlogix.inventario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InventarioApplicationTests {
 
 	@Test
