420fda4fd3c9169d9eb15d436afdd0f0f0467b58
dainz <mbenjaminarancibia@gmail.com>
2026-03-29 01:33:53 -0300

    pass docker spring profile inside image as default

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM eclipse-temurin:17-jre
 WORKDIR /app
 COPY target/inventario.jar app.jar
 EXPOSE 9090
-CMD ["java", "-jar", "app.jar"]
+CMD ["java", "-jar", "app.jar", "--spring.profiles.active=docker"]
 
 # Create image
 # docker build -t xdainz/api-inventario .
--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ docker network create mi_red
 
 docker run -d --name inventario-db --network mi_red -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=db_inventario mysql:latest 
 
-docker run -d --name inventario --network mi_red -p 9090:9090 --restart on-failure -e SPRING_PROFILES_ACTIVE=docker xdainz/api-inventario:latest
+docker run -d --name inventario --network mi_red -p 9090:9090 --restart on-failure xdainz/api-inventario:latest
